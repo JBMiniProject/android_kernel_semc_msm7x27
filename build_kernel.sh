30b468734d950a7c1a3d65589e54afb2b280d5b5
Daveee10 <daver16@msn.com>
2012-09-23 13:20:43 +0200

    Remove auto cleaning from build script because it delete wifi modules from compiled dir

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,6 +10,7 @@ FROM=`pwd`
 
 # Deleting previous output
 rm -f compiled/*
+mkdir compiled
 
 # Build the kernel
 clear
@@ -33,23 +34,15 @@ cp *.ko $FROM/compiled/
 popd
 ENDW=$(date +%s)
 
-# Cleaning (mrproper)
-STARTC=$(date +%s)
-make mrproper
-ENDC=$(date +%s)
-
 # Calculating the times
 ELAPSEDK=$((ENDK - STARTK))
 ELAPSEDW=$((ENDW - STARTW))
-ELAPSEDC=$((ENDC - STARTC))
-ELAPSEDT=$((ENDC - STARTK))
+ELAPSEDT=$((ENDW - STARTK))
 EK_MIN=$((ELAPSEDK / 60))
 EW_MIN=$((ELAPSEDW / 60))
-EC_MIN=$((ELAPSEDC / 60))
 ET_MIN=$((ELAPSEDT / 60))
 EK_SEC=$((ELAPSEDK - EK_MIN * 60))
 EW_SEC=$((ELAPSEDW - EW_MIN * 60))
-EC_SEC=$((ELAPSEDC - EC_MIN * 60))
 ET_SEC=$((ELAPSEDT - ET_MIN * 60))
 
 # Printing the summary
@@ -62,10 +55,6 @@ echo -n "[*]-------->>>> "
 printf "Wifi modules building time: "
 [ $EW_MIN != 0 ] && printf "%d min(s) " $EW_MIN
 printf "%d sec(s)\n" $EW_SEC
-echo -n "[*]-------->>>> "
-printf "Cleaning time: "
-[ $EC_MIN != 0 ] && printf "%d min(s) " $EC_MIN
-printf "%d sec(s)\n" $EC_SEC
 echo -n "[*]-------->>>> "
 printf "Elapsed from start: "
 [ $ET_MIN != 0 ] && printf "%d min(s) " $ET_MIN
